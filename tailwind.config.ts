fcd81d9090ca4157da56193c2f1734fa1785a231
ECarry <lianshiliang93@gmail.com>
2023-12-18 22:09:03 +0800

    style: customize Tailwind screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,12 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "xs": "475px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+        "2xl": "1536px",
       },
     },
     extend: {
