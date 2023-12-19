b8cabff46fc5ae61cd15f9b5d48e71c57e19e64f
ecarry <lianshiliang93@gmail.com>
2023-12-19 17:53:52 +0800

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Server Hub',
+  title: 'Server Hub - DATA & NEXUS',
   description: 'DATA & NEXUS',
 }
 
