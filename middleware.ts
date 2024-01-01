213befd4ac61a4b59e3b61b96c7414dabdff4fde
ECarry <lianshiliang93@gmail.com>
2024-01-01 21:45:02 +0800

    feat: user model & nextauth config

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { auth } from "@/lib/auth"
+
+export default auth((req) => {
+  // req.auth
+})
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
