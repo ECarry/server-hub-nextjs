4d2d8369687da0bfa09a09f2b7f21d8f490fcb3d
ECarry <lianshiliang93@gmail.com>
2024-01-02 22:56:59 +0800

    feat: login form & page

--- app/(home)/(auth)/login/page.tsx
+++ app/(home)/(auth)/login/page.tsx
@@ -1,8 +1,13 @@
+import LoginForm from '@/components/login-form'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='container space-y-8'>
+      <h1 className='text-3xl font-semibold'>Login</h1>
+      <h2>Don&apos;t have an account? Create here</h2>
+      <LoginForm />
+    </div>
   )
 }
 
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,3 @@
-import { Separator } from '@radix-ui/react-separator'
 import Footer from './_components/footer'
 import Header from './_components/header'
 import { NavigationBar } from './_components/navigation-bar'
