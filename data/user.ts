a60badbb729d05888765d8084bf8b61fadface25
ecarry <lianshiliang93@gmail.com>
2024-01-04 10:47:35 +0800

    style: code style

--- data/user.ts
+++ data/user.ts
@@ -1,11 +1,11 @@
 import { db } from "@/lib/db"
 
 export const getUserByEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email
-    }
-  })
-
-  return user
+  try {
+    const user = await db.user.findUnique({ where: {email} })
+  
+    return user
+  } catch (error) {
+    return null
+  }
 }
