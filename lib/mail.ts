9edc989a950ebde9c452eff3c36794c0806dcafe
ECarry <lianshiliang93@gmail.com>
2024-01-05 21:04:01 +0800

    fix: confirmLink NODE_EVN

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,9 @@ export const sendVerificationEmail = async (
   email: string,
   token: string,
 ) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+  const confirmLink = process.env.NODE_ENV === 'production' 
+  ? `http://server-hub.net/auth/new-verification?token=${token}`
+  : `http://localhost:3000/auth/new-verification?token=${token}`
 
   await resend.emails.send({
     from: 'admin@server-hub.net',
