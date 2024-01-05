4fba652d404f6ff2dc5d31aa69cd151e19014061
ECarry <lianshiliang93@gmail.com>
2024-01-06 00:23:03 +0800

    fix: Type error

--- actions/reset.ts
+++ actions/reset.ts
@@ -7,30 +7,26 @@ import { ResetPasswordSchema } from '@/schemas'
 import * as z from 'zod'
 
 export const resetPassword = async (values: z.infer<typeof ResetPasswordSchema>) => {
-  try {
-    const validatedFields = ResetPasswordSchema.safeParse(values)
+  const validatedFields = ResetPasswordSchema.safeParse(values)
 
-    if (!validatedFields.success) {
-      return { error: 'Invalid email!' }
-    }
-
-    const { email } = validatedFields.data
-
-    const user = await getUserByEmail(email)
-
-    if (!user) {
-      return { error: 'Email does not exist!' }
-    }
-
-    const resetPasswordToken = await generatePasswordResetToekn(email)
-
-    await sendRestPasswordEmail(
-      resetPasswordToken.email,
-      resetPasswordToken.token
-    )
-
-    return { success: 'Reset email sent!' }
-  } catch (error) {
-    return { error }
+  if (!validatedFields.success) {
+    return { error: 'Invalid email!' }
   }
+
+  const { email } = validatedFields.data
+
+  const user = await getUserByEmail(email)
+
+  if (!user) {
+    return { error: 'Email does not exist!' }
+  }
+
+  const resetPasswordToken = await generatePasswordResetToekn(email)
+
+  await sendRestPasswordEmail(
+    resetPasswordToken.email,
+    resetPasswordToken.token
+  )
+
+  return { success: 'Reset email sent!' }
 }
