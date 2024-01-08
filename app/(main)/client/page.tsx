7ae999b12dbf0e4101da0f8e2df99fa48aa9b3e5
ecarry <lianshiliang93@gmail.com>
2024-01-08 16:36:11 +0800

    feat: client page use current user

--- app/(main)/client/page.tsx
+++ app/(main)/client/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+'use client'
+
+import { useCurrentUser } from "@/hooks/use-current-user"
 
 const ClientPage = () => {
+  const user = useCurrentUser()
+
   return (
-    <div>ClientPage</div>
+    <div>
+      <h1>ClientPage</h1>
+      {JSON.stringify(user)}
+    </div>
   )
 }
 
