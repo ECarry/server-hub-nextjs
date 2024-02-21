ec3b7d09884154759c7e898f202cb62942ee37a0
ecarry <lianshiliang93@gmail.com>
2024-02-21 16:46:30 +0800

    style: code style

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { db } from "@/lib/db"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
 
-import authConfig from "@/auth.config"
-import { UserRole } from "@prisma/client"
-import { getUserById } from "@/data/user"
-import { getAccountByUserId } from "@/data/account"
+import authConfig from "@/auth.config";
+import { UserRole } from "@prisma/client";
+import { getUserById } from "@/data/user";
+import { getAccountByUserId } from "@/data/account";
 
 // https://authjs.dev/getting-started/typescript
 declare module "@auth/core/types" {
@@ -13,64 +13,59 @@ declare module "@auth/core/types" {
     user: {
       id: string;
       role: UserRole;
-    } & DefaultSession["user"] 
+    } & DefaultSession["user"];
   }
 }
 
-export const { 
+export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
   pages: {
-    signIn: '/auth/login',
-    error: '/auth/error',
+    signIn: "/auth/login",
+    error: "/auth/error",
   },
   callbacks: {
     async signIn({ user, account }) {
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
 
-      console.log({
-        USER: user,
-        ACCOUNT: account
-      });
-      
-      const existingUser = await getUserById(user.id)
+      const existingUser = await getUserById(user.id);
 
-      if (!existingUser || !existingUser.emailVerified) return false
-      
-      return true
+      if (!existingUser || !existingUser.emailVerified) return false;
+
+      return true;
     },
     async session({ session, user, token }) {
       if (token.sub && session.user) {
-        session.user.id = token.sub
+        session.user.id = token.sub;
       }
 
       if (token.role && session.user) {
-        session.user.role = token.role as UserRole
+        session.user.role = token.role as UserRole;
       }
 
-      return session
+      return session;
     },
     async jwt({ token }) {
-      if (!token.sub) return token
+      if (!token.sub) return token;
 
-      const existingUser = await db.user.findUnique({ where: { id: token.sub }})
+      const existingUser = await db.user.findUnique({
+        where: { id: token.sub },
+      });
 
-      if (!existingUser) return token
+      if (!existingUser) return token;
 
-      const existingAccount = await getAccountByUserId(
-        existingUser.id
-      );
-      
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
       token.isOAuth = !!existingAccount;
-      token.role = existingUser.role
-      return token
-    }
+      token.role = existingUser.role;
+      return token;
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
-})
+});
