feaba14bb535e53d793665c47a517509512a3218
ECarry <lianshiliang93@gmail.com>
2024-02-25 21:59:01 +0800

    fix

--- components/hover-card-content.tsx
+++ components/hover-card-content.tsx
@@ -5,11 +5,14 @@ export const ScreensContent = ({ name }: { name: string }) => {
     <div className="flex flex-col gap-y-3 w-full">
       <div className="grid gap-3 rounded-2xl p-4 bg-foreground/5 dark:bg-foreground/10 grid-cols-3 w-full">
         {Array.from({ length: 3 }).map((_, index) => (
-          <div key={index} className="overflow-hidden rounded-lg h-auto max-h-56 w-full">
+          <div
+            key={index}
+            className="overflow-hidden rounded-lg h-auto max-h-56 w-full"
+          >
             <Image
               className="h-full w-full object-cover"
-              src="/images/phone-screen.webp"
-              alt="Phone Screen App"
+              src=""
+              alt=""
               width={400}
               height={863}
               priority
@@ -33,8 +36,8 @@ export const WebContent = ({ name }: { name: string }) => {
         <div className="overflow-hidden rounded-lg max-h-56 h-auto w-full">
           <Image
             className="h-auto w-full object-cover"
-            src="/images/desktop-screen.webp"
-            alt="Phone Screen App"
+            src=""
+            alt=""
             width={1256}
             height={863}
           />
