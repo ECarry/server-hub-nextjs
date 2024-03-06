95d95e6411b741f315223202fdae49eaed14fe4e
ecarry <lianshiliang93@gmail.com>
2024-03-06 16:12:54 +0800

    style: login error page

--- app/(auth)/auth/error/page.tsx
+++ app/(auth)/auth/error/page.tsx
@@ -1,10 +1,15 @@
+import CardWrapper from "@/components/auth/card-wrapper";
 
 const ErrorPage = () => {
   return (
-    <div>
-      error
-    </div>
-  )
-}
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="w-full flex justify-center items-center"></div>
+    </CardWrapper>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
