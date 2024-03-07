58dfc298e5b09d51e938c14646be7aa301724981
ECarry <lianshiliang93@gmail.com>
2024-03-07 22:06:04 +0800

    feat: dashboard navbar

---
+++ app/(dasboard)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <header>
+      <h1>Navbar</h1>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/(dasboard)/layout.tsx
+++ app/(dasboard)/layout.tsx
@@ -2,13 +2,20 @@ import { auth } from "@/auth";
 import RoleGate from "@/components/auth/role-gate";
 import { redirect } from "next/navigation";
 
+import Navbar from "./_components/navbar";
+
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
   if (!session) {
     redirect("/auth/login");
   }
 
-  return <RoleGate allowedRole="ADMIN">{children}</RoleGate>;
+  return (
+    <RoleGate allowedRole="ADMIN">
+      <Navbar />
+      {children}
+    </RoleGate>
+  );
 };
 
 export default DashboardLayout;
