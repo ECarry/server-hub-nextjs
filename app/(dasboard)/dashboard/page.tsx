febbdcd3002ec4d86b3877875a2433aa8e99241c
ecarry <lianshiliang93@gmail.com>
2024-03-08 10:37:14 +0800

    feat: add Input OTP component

--- app/(dasboard)/dashboard/page.tsx
+++ app/(dasboard)/dashboard/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <InputOTP
+        maxLength={6}
+        render={({ slots }) => (
+          <>
+            <InputOTPGroup>
+              {slots.slice(0, 6).map((slot, index) => (
+                <InputOTPSlot key={index} {...slot} />
+              ))}{" "}
+            </InputOTPGroup>
+          </>
+        )}
+      />
+    </div>
+  );
 };
 
 export default DashboardPage;
