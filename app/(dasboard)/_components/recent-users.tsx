a1643fc80e9f084184be255262ea7592d9fe1c0d
ecarry <lianshiliang93@gmail.com>
2024-03-11 15:54:30 +0800

    style: avatar fallback style

--- app/(dasboard)/_components/recent-users.tsx
+++ app/(dasboard)/_components/recent-users.tsx
@@ -10,7 +10,7 @@ const Row = (user: User) => {
     <div className="flex items-center">
       <Avatar className="h-9 w-9">
         <AvatarImage src={user.image || ""} alt="Avatar" />
-        <AvatarFallback>{user.name}</AvatarFallback>
+        <AvatarFallback>{user.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
       </Avatar>
       <div className="ml-4 space-y-1">
         <p className="text-sm font-medium leading-none">{user.name}</p>
