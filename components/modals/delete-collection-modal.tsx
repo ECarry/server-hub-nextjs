65223c69ea3045b5d29d5c0364cfecccc41b952c
ECarry <lianshiliang93@gmail.com>
2024-03-20 14:24:20 +0800

    refactor: use alert dialog

--- components/modals/delete-collection-modal.tsx
+++ components/modals/delete-collection-modal.tsx
@@ -13,6 +13,15 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import FormError from "../auth/form-error";
 import { deleteCollection } from "@/actions/collection";
 import { Loader2 } from "lucide-react";
@@ -46,30 +55,22 @@ const DeleteCollectionModal = () => {
   };
 
   return (
-    <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader className="space-y-8">
-          <DialogTitle className=" text-center text-xl truncate font-semibold">
-            Are you sure?
-          </DialogTitle>
-          <DialogDescription className="text-center text-gray-500">
+    <AlertDialog open={isModalOpen} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
             Deleting your collection is permanent and irreversible.
-          </DialogDescription>
-        </DialogHeader>
-
+          </AlertDialogDescription>
+        </AlertDialogHeader>
         <FormError message={error} />
-        <DialogFooter className="mt-4 grid grid-cols-2 gap-x-3">
-          <Button
-            variant={"outline"}
-            onClick={() => onClose()}
-            disabled={isPending}
-          >
-            Cancel
-          </Button>
+        <AlertDialogFooter>
+          <AlertDialogCancel disabled={isPending}>Cancel</AlertDialogCancel>
           <Button
             variant={"destructive"}
             onClick={onSubmit}
             disabled={isPending}
+            className="w-[160px]"
           >
             {isPending ? (
               <Loader2 className="mr-2 h-4 w-4 animate-spin" />
@@ -77,9 +78,9 @@ const DeleteCollectionModal = () => {
               <span>Delete collection</span>
             )}
           </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
