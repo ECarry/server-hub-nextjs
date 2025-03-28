389012bb15f1e072f156ccb4f42516c76be08bf4
ecarry <lianshiliang93@gmail.com>
2025-03-28 12:20:23 +0800

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TRPCProvider>
-          {children}
-          <Toaster />
-        </TRPCProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <TRPCProvider>
+            {children}
+            <Toaster />
+          </TRPCProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
