8833579d17aa37f6ad5d0c94a07096b71671e74b
ecarry <lianshiliang93@gmail.com>
2025-03-30 22:13:53 +0800

    chore: brand logo loading skeleton

--- src/modules/brands/ui/sections/brands-table-section/index.tsx
+++ src/modules/brands/ui/sections/brands-table-section/index.tsx
@@ -9,6 +9,15 @@ import { trpc } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { IconPlus } from "@tabler/icons-react";
 import { BrandCreateModal } from "../../components/brand-create-modal";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const BrandsTableSection = () => {
   return (
@@ -21,7 +30,42 @@ export const BrandsTableSection = () => {
 };
 
 const BrandsTableSectionSkeleton = () => {
-  return <div className="h-96 animate-pulse bg-muted" />;
+  return (
+    <div className="w-full flex-col justify-start space-y-6">
+      <div className="flex items-center px-4 lg:px-6">
+        <h1 className="text-2xl font-bold">Brands</h1>
+        <div className="flex ml-auto items-center gap-2">
+          <Skeleton className="h-8 w-24" />
+        </div>
+      </div>
+      <div className="relative flex flex-col gap-4 overflow-auto px-4 lg:px-6 mt-4">
+        <div className="overflow-hidden rounded-lg border">
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader className="bg-muted sticky top-0 z-10">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <TableHead key={index}>
+                    <Skeleton className="h-8 w-24" />
+                  </TableHead>
+                ))}
+              </TableHeader>
+              <TableBody className="**:data-[slot=table-cell]:first:w-8">
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <TableRow key={index}>
+                    {Array.from({ length: 5 }).map((_, cellIndex) => (
+                      <TableCell key={cellIndex}>
+                        <Skeleton className="h-8 w-24" />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const BrandsTableSectionSuspense = () => {
@@ -32,6 +76,7 @@ const BrandsTableSectionSuspense = () => {
     <div className="w-full flex-col justify-start space-y-6">
       <BrandCreateModal open={isOpen} onOpenChange={setIsOpen} />
       <div className="flex items-center px-4 lg:px-6">
+        <h1 className="text-2xl font-bold">Brands</h1>
         <div className="flex ml-auto items-center gap-2">
           <Button variant="outline" size="sm" onClick={() => setIsOpen(true)}>
             <IconPlus />
