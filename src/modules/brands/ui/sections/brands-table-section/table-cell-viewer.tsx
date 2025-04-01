0250f630e2cff3091b1b4084318cd31b5931ded0
ecarry <lianshiliang93@gmail.com>
2025-04-01 14:29:37 +0800

    chore: TableCellViewer component

---
+++ src/modules/brands/ui/sections/brands-table-section/table-cell-viewer.tsx
@@ -0,0 +1,74 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+  SheetTrigger,
+  SheetClose,
+  SheetFooter,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { brandsSelectSchema } from "@/db/schema";
+import { TrendingUpIcon } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export const TableCellViewer = ({
+  item,
+}: {
+  item: z.infer<typeof brandsSelectSchema>;
+}) => {
+  const isMobile = useIsMobile();
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="link" className="w-fit px-0 text-left text-foreground">
+          {item.name}
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right" className="flex flex-col">
+        <SheetHeader className="gap-1">
+          <SheetTitle>{item.name}</SheetTitle>
+          <SheetDescription>
+            Showing total visitors for the last 6 months
+          </SheetDescription>
+        </SheetHeader>
+        <div className="flex flex-1 flex-col gap-4 overflow-y-auto py-4 text-sm">
+          {!isMobile && (
+            <>
+              <div className="grid gap-2">
+                <div className="flex gap-2 font-medium leading-none">
+                  Trending up by 5.2% this month{" "}
+                  <TrendingUpIcon className="size-4" />
+                </div>
+                <div className="text-muted-foreground">
+                  Showing total visitors for the last 6 months. This is just
+                  some random text to test the layout. It spans multiple lines
+                  and should wrap around.
+                </div>
+              </div>
+            </>
+          )}
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-3">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue={item.name} />
+            </div>
+          </form>
+        </div>
+        <SheetFooter className="mt-auto flex gap-2 sm:flex-col sm:space-x-0">
+          <Button className="w-full">Submit</Button>
+          <SheetClose asChild>
+            <Button variant="outline" className="w-full">
+              Done
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
