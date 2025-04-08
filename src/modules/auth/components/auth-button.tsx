eec5eeab9bf2835753b9a7dfb07070836f66afa4
ecarry <lianshiliang93@gmail.com>
2025-04-08 22:19:12 +0800

    chore: auth button

--- src/modules/auth/components/auth-button.tsx
+++ src/modules/auth/components/auth-button.tsx
@@ -10,13 +10,9 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import {
-  LogOut,
-  Loader2,
-  UserCircleIcon,
-  ClapperboardIcon,
-} from "lucide-react";
+import { LogOut, Loader2, Bookmark } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const AuthButton = () => {
   const router = useRouter();
@@ -25,14 +21,14 @@ const AuthButton = () => {
 
   if (!session) {
     return (
-      <Button
-        variant="outline"
-        className="px-4 py-2 text-sm font-medium text-blue-600 hover:bg-blue-500 border-blue-500/70 rounded-full shadow-none [&_svg]:size-4"
-        onClick={() => router.push("/sign-in")}
-      >
-        <UserCircleIcon />
-        Sign in
-      </Button>
+      <div className="hidden lg:flex flex-row items-center gap-3">
+        <Button variant="ghost" size="lg" asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+        <Button variant="default" size="lg" asChild>
+          <Link href="/login">Create free account</Link>
+        </Button>
+      </div>
     );
   }
 
@@ -48,58 +44,51 @@ const AuthButton = () => {
           <AvatarFallback>{session.user.name.charAt(0)}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem asChild>
-          <button
-            className="w-full"
-            onClick={() => router.push(`/users/${session.user.id}`)}
-          >
-            <span className="text-sm">
-              <div className="flex items-center gap-2">
-                <UserCircleIcon className="size-4" />
-                My Profile
-              </div>
-            </span>
-          </button>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <button className="w-full" onClick={() => router.push("/studio")}>
-            <span className="text-sm">
-              <div className="flex items-center gap-2">
-                <ClapperboardIcon className="size-4" />
-                Studio
-              </div>
-            </span>
-          </button>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <button
-            className="w-full"
-            onClick={async () => {
-              setIsSignOut(true);
-              await signOut({
-                fetchOptions: {
-                  onSuccess() {
-                    router.push("/");
-                  },
-                },
-              });
-              setIsSignOut(false);
-            }}
-            disabled={isSignOut}
-          >
-            <span className="text-sm">
-              {isSignOut ? (
-                <Loader2 size={15} className="animate-spin" />
-              ) : (
+      <DropdownMenuContent align="end" className="w-60  backdrop-blur p-[8px]">
+        <div className="flex flex-col pt-2 px-2.5">
+          <h1 className="truncate ">{session.user.name}</h1>
+          <p className="text-sm text-muted-foreground">{session.user.email}</p>
+        </div>
+        <div className="flex flex-col gap-2 py-2">
+          <DropdownMenuItem asChild>
+            <button>
+              <span className="w-full">
                 <div className="flex items-center gap-2">
-                  <LogOut size={16} />
-                  Sign Out
+                  <Bookmark size={20} />
+                  Saved
                 </div>
-              )}
-            </span>
-          </button>
-        </DropdownMenuItem>
+              </span>
+            </button>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <button
+              className="w-full"
+              onClick={async () => {
+                setIsSignOut(true);
+                await signOut({
+                  fetchOptions: {
+                    onSuccess() {
+                      router.push("/");
+                    },
+                  },
+                });
+                setIsSignOut(false);
+              }}
+              disabled={isSignOut}
+            >
+              <span className="w-full">
+                {isSignOut ? (
+                  <Loader2 size={20} className="animate-spin" />
+                ) : (
+                  <div className="flex items-center gap-2">
+                    <LogOut size={20} />
+                    Sign Out
+                  </div>
+                )}
+              </span>
+            </button>
+          </DropdownMenuItem>
+        </div>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -2,14 +2,11 @@ import Link from "next/link";
 import { NavLinks } from "./nav-links";
 import { NavMenu } from "./nav-menu";
 import { SearchButtonNav } from "./search-button-nav";
-import { getCurrentUser } from "@/modules/auth/lib/get-current-user";
-import { Button } from "@/components/ui/button";
-import { UserAvatar } from "@/components/user-avatar";
+
 import Image from "next/image";
+import AuthButton from "@/modules/auth/components/auth-button";
 
 export const HomeNavbar = async () => {
-  const { user } = await getCurrentUser();
-
   return (
     <header className="z-50 sticky top-0 flex h-[100px] border-b md:border-transparent md:h-[72px] bg-background">
       <div className="w-full grid grid-cols-[min-content_auto_min-content] grid-rows-[1fr_auto] items-center gap-x-2 md:grid-cols-[1fr_minmax(auto,500px)_1fr] lg:grid-rows-1 px-5 sm:px-6 md:px-8 lg:px-12 xl:px-20">
@@ -28,18 +25,7 @@ export const HomeNavbar = async () => {
 
         <div className="shrink-0 items-end">
           <div className="flex flex-row items-center justify-end gap-3">
-            {!user ? (
-              <div className="hidden lg:flex flex-row items-center gap-3">
-                <Button variant="ghost" size="lg" asChild>
-                  <Link href="/login">Log in</Link>
-                </Button>
-                <Button variant="default" size="lg" asChild>
-                  <Link href="/login">Create free account</Link>
-                </Button>
-              </div>
-            ) : (
-              <UserAvatar name={user.name} imageUrl={user.image || ""} />
-            )}
+            <AuthButton />
             <NavMenu />
           </div>
         </div>
